e9cb50e19269e70e67a47cf1b89919c2f4efc224
Art Tsai <arttsai@gmail.com>
2019-11-05 20:36:49 +0800

    split 2 components ( CalendarWeek, CalendarEntry ) from app.vue

---
+++ 002-calendar-app-cli/src/components/CalendarEntry.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="calendar-entry">
+      <!-- markup for calendar entry  -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarEntry',
+}
+</script>
+
+<style lang="scss">
+#calendar-entry {
+
+}
+</style>
---
+++ 002-calendar-app-cli/src/components/CalendarWeek.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="calendar-week" class="container">
+      <!-- markup for calendar week -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarWeek',
+}
+</script>
+
+<style lang="scss">
+#calendar-week {
+
+}
+</style>
