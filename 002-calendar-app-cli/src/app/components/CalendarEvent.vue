7176ca3035605ef383cff33295697c56258b43e9
Art Tsai <arttsai@gmail.com>
2019-11-13 17:33:28 +0800

    init from command: vue ui

--- 002-calendar-app-cli/src/app/components/CalendarEvent.vue
+++ 002-calendar-app-cli/src/app/components/CalendarEvent.vue
@@ -1,18 +1,35 @@
 <template>
     <div class="day-event" :style="getEventBackgroundColor">
-        <div>
+        <div v-if="!event.edit">
             <span class="has-text-centered details">{{ event.details }}</span>
             <div class="has-text-centered icons">
-                <i class="fa fa-pencil-square edit-icon"></i>
-                <i class="fa fa-trash-o delete-icon"></i>
+                <i class="fa fa-pencil-square edit-icon"
+                    @click="editEvent(day.id, event.details)"></i>
+                <i class="fa fa-trash-o delete-icon"
+                    @click="deleteEvent(day.id, event.details)"></i>
+            </div>
+        </div>
+        <div v-if="event.edit">
+            <input type="text" :placeholder="event.details"
+                v-model="newEventDetails" />
+            <div class="has-text-centered icons">
+                <i class="fa fa-check" 
+                    @click="updateEvent(day.id, event.details, newEventDetails)"></i>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { store } from "../store.js";
+
 export default {
     name: "CalendarEvent",
+    data() {
+        return {
+            newEventDetails:''
+        }
+    },  
     props: ['event', 'day'],
     computed: {
         getEventBackgroundColor() {
@@ -20,6 +37,19 @@ export default {
             let randomColor = 
                 colors[Math.floor(Math.random()*colors.length)];
             return `background-color: ${randomColor}`; 
+        }
+    },
+    methods: {
+        editEvent(dayId, eventDetails) {
+            store.editEvent(dayId, eventDetails);
+        },
+        updateEvent(dayId, originalEventDetails, newEventDetails) {
+            if (newEventDetails === '') newEventDetails = originalEventDetails;
+            store.updateEvent(dayId, originalEventDetails, newEventDetails);
+            this.newEventDetails = ''; 
+        },
+        deleteEvent(dayId, eventDetails) {
+            store.deleteEvent(dayId, eventDetails);
         }
     }
 }
